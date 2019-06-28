bff4ffc9476892a4d442b0af66130433e382ea21
Piyush Balapure <piyush.balapure@cuelogic.co.in>
2019-06-28 15:36:29 +0530

    changes for hosting

---
+++ build/precache-manifest.233d43bd73cb8a7ace585b0abf6c4f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0f4d2ddd163fe6f517666336c8138e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51dbe405146fb138290e",
+    "url": "/static/css/main.82c993d0.chunk.css"
+  },
+  {
+    "revision": "3cb03fd24864cd9e240d",
+    "url": "/static/js/2.cce8fc17.chunk.js"
+  },
+  {
+    "revision": "17ad2b1e9fda45dd0241",
+    "url": "/static/js/3.6202dcf1.chunk.js"
+  },
+  {
+    "revision": "51dbe405146fb138290e",
+    "url": "/static/js/main.ad526752.chunk.js"
+  },
+  {
+    "revision": "f8631b0bb4d5e57a1521",
+    "url": "/static/js/runtime~main.9a2822f8.js"
+  }
+]);
